f9f45acc7b114bbc3610ebf72592303e43d4dfbc
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-16 19:50:43 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+function findMatching(arr, str) {
+  let names = []
+  let newStr = str.toUpperCase()
+  arr.filter(function (el) {
+    let newEl = el.toUpperCase()
+    if (newEl === newStr) {
+      names.push(el)
+    }
+  })
+  return names
+}
+
+
+function fuzzyMatch(arr, query) {
+  let names = []
+  let x = query.length
+  arr.filter(function(el) {
+    let check = el.slice(0,x)
+    if (check === query) {
+      names.push(el)
+    }})
+    return names
+}
+
+function matchName(driver, str) {
+  let names = []
+  driver.filter(function (el) {
+    let drivName = el.name
+    if (drivName === str) {
+      names.push(el)
+    }
+  })
+  return names
+}
+
+
+drivers = [
+  'Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'
+]
+
+fuzzyMatch(drivers, 'Sa')
